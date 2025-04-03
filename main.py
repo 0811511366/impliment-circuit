fc6853bf7bcb5ecdb9a15304da80c87c0a042d25
0811511366 <accmughal@gmail.com>
2025-04-03 15:22:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+num1 = 18
+num2 = 6
+print("num1 & num2 are:", num1, num2)
+print("num1 | num2 are:", num1 | num2)
+print("num1 ^ num2 are:", num1 ^ num2)
+print("num1 << num2 are:", num1 << num2)
+print("num1 >> num2 are:", num1 >> num2)
+print("~num1 are:", ~num1)
+print("~num2 are:", ~num2)
